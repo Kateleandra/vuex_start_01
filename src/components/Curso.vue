66d9b55dc1bd1864aa9f0698f2b6af99185e8fab
kate <kateleandra@hotmail.com>
2019-11-09 11:25:07 -0300

    Iniciando vuex, inserindo mapState

---
+++ src/components/Curso.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <p>Cursos</p>
+    <p>Aulas completas: {{aulas}}</p>
+</div>
+</template>
+<script>
+import {mapState} from 'vuex';
+
+export default{
+    data(){
+        return{
+            
+        }
+    },
+    computed: mapState(["aulas"]),
+    methods:{
+        
+    }
+}
+    
+</script>
+<style>
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>User {{user}}</p>
+    
+</template>
+<script>
+import {mapState} from 'vuex';
+
+export default{
+    data(){
+        return{
+            
+        }
+    },
+    computed: mapState(["user"]),
+    methods:{
+        
+    }
+}
+    
+</script>
+<style>
+
+</style>
